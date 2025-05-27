3d2d20affbd73d49014496683f1669c2f6a6a0cb
seocom <wjddnjs6520@naver.com>
2025-05-27 10:43:54 +0900

    Day1 Substract
    두 수의 차

---
+++ Day1/Substract/Substract/Substract.cpp
@@ -0,0 +1,19 @@
+﻿#include <stdio.h>
+#include <stdbool.h>
+#include <stdlib.h>
+#include <iostream>
+
+
+int solution(int num1, int num2) {
+    int answer = 0;
+    answer = num1 - num2;
+    return answer;
+}
+
+int main() {
+    
+    std::cout<<solution(2, 3);
+    std::cout << solution(100, 2);
+
+    return 0;
+}
