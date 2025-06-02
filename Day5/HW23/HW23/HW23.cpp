121cc7eb4bc0412d58591c2cebf1c52135f7c7ee
seocom <wjddnjs6520@naver.com>
2025-06-02 21:15:12 +0900

    Day5
    struct 연습

---
+++ Day5/HW23/HW23/HW23.cpp
@@ -0,0 +1,36 @@
+﻿#include <iostream>
+#include <vector>
+#include <map>
+
+using namespace std;
+
+int main() {
+    // 벡터와 맵 데이터 정의
+    vector<int> vec = { 10, 20, 30, 40, 50 };
+    map<string, int> mp = {
+        {"Alice", 90},
+        {"Bob", 85},
+        {"Charlie", 95}
+    };
+
+    for (vector<int>::iterator it = vec.begin(); it != vec.end(); ++it) {
+        cout << *it << " ";
+    }
+    cout << endl;
+
+    for (map<string, int>::iterator it = mp.begin(); it != mp.end(); ++it) {
+        cout << it->first << " : " << it->second << " ";
+    }
+
+    for (vector<int>::const_reverse_iterator it = vec.rbegin(); it != vec.rend(); ++it) {
+        cout << *it << " "; 
+    }
+    cout << endl;
+
+    for (map<string, int>::const_reverse_iterator it = mp.rbegin(); it != mp.rend(); ++it) {
+        cout << it->first << " : " << it->second << " ";
+    }
+    
+
+    return 0;
+}
---
+++ Struct/HW241/HW241/HW241.cpp
@@ -0,0 +1,52 @@
+﻿#include <iostream>
+#include <string>
+
+using namespace std;
+
+class Student {
+private:
+    string name;
+    int age;
+public:
+    Student(string name, int age) 
+    {
+        this->name = name;
+        this->age = age;
+    }
+
+    void saveImpo() 
+    {
+        cout << "impo is Save!!" << endl;
+    }
+
+    string getImpo() {
+        string impo = "";
+        impo += name;
+        impo += " is age: ";
+        impo += to_string(age);
+        return impo;
+    }
+};
+
+
+class StudentPrinter 
+{
+private:
+
+
+public:
+
+    void print(Student& student) {
+        cout << student.getImpo() << endl;
+    }
+
+};
+
+int main()
+{
+    Student s("SJW", 25);
+
+    StudentPrinter sp;
+
+    sp.print(s);
+}
