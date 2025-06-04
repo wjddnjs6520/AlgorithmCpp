80b96f7a7d2fe069e97ee7895caffa1952f58741
seocom <wjddnjs6520@naver.com>
2025-06-04 12:58:08 +0900

    Day6
    Solution 52~54 clear

---
+++ Day6/Solution53/Solution53/Solution53.cpp
@@ -0,0 +1,52 @@
+﻿#include <iostream>
+#include <string>
+#include <vector>
+
+using namespace std;
+
+
+vector<int> solution(int k, vector<int> score) {
+    vector<int> answer;
+    vector<int> karr;
+
+    for (int i = 0; i < score.size(); i++) {
+        bool inserted = false;
+        if (karr.empty()) {
+            karr.push_back(score[i]);
+        }
+        else {
+            for (int j = 0; j < karr.size(); j++) {
+                if (score[i] >= karr[j]) {
+                    karr.insert(karr.begin() + j, score[i]);
+                    inserted = true;
+                    break;
+                }
+            }
+            if (!inserted) {
+                karr.push_back(score[i]);
+            }
+        }  
+        if (karr.size() > k) {
+            karr.pop_back();
+        }
+        answer.push_back(karr.back());
+    }
+    return answer;
+}
+
+int main()
+{
+    vector<int> v1 = { 10, 100, 20, 150, 1, 100, 200 };
+    vector<int> a1 = solution(3, v1);
+    vector<int> v2 = {0, 300, 40, 300, 20, 70, 150, 50, 500, 1000};
+    vector<int> a2 = solution(4, v2);
+    
+    for (int a : a1) {
+        cout << a << " ";
+    }
+    cout << endl;
+    for (int a : a2) {
+        cout << a << " ";
+    }
+
+}
---
+++ Day6/Solution54/Solution54/Solution54.cpp
@@ -0,0 +1,66 @@
+﻿#include <iostream>
+#include <string>
+#include <vector>
+
+using namespace std;
+
+string solution(int a, int b) {
+    string answer = "";
+    int day = b;
+    while (a > 1) {
+        switch (a - 1) {
+        case 1:
+        case 3:
+        case 5:
+        case 7:
+        case 8:
+        case 10:
+        case 12:
+            day += 31;
+            a--;
+            break;
+        case 4:
+        case 6:
+        case 9:
+        case 11:
+            day += 30;
+            a--;
+            break;
+        case 2:
+            day += 29;
+            a--;
+            break;
+        }
+    }
+
+    switch (day % 7) {
+    case 1:
+        answer = "FRI";
+        break;
+    case 2:
+        answer = "SAT";
+        break;
+    case 3:
+        answer = "SUN";
+        break;
+    case 4:
+        answer = "MON";
+        break;
+    case 5:
+        answer = "TUE";
+        break;
+    case 6:
+        answer = "WED";
+        break;
+    case 0:
+        answer = "THU";
+        break;
+    }
+
+    return answer;
+}
+
+int main()
+{
+    cout << solution(5, 24);
+}
